88f09f4ea6a7ee2065938edc616baf92aaec9465
Boris <boris201113@rambler.ru>
2015-02-11 11:40:00 +0200

    додано фукцію виводу товарів

--- box2.php
+++ box2.php
@@ -6,3 +6,15 @@
  * Time: 10:09
  */
 echo 'Hello world';
+* User: Боря
+* Date: 11.02.2015
+* Time: 10:09
+*/
+echo 'Hello world';
+ */
+echo 'Hello world';
+* User: Боря
+* Date: 11.02.2015
+* Time: 10:09
+*/
+echo 'Hello world';
